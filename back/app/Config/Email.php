c4c3f8b6c92c33bb341febdb60a6e10afa3e67c9
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2023-06-13 22:12:20 +0200

    Allow to set email setting in env

--- back/app/Config/Email.php
+++ back/app/Config/Email.php
@@ -6,112 +6,112 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = $_ENV['email.from'] ?? 'commuse@example.com';
+    public string $fromName   = $_ENV['email.from_name'] ?? 'commuse';
     public string $recipients = '';
 
     /**
      * The "user agent"
      */
-    public string $userAgent = 'commuse';
+    public string $userAgent = $_ENV['email.user_agent'] ?? 'commuse';
 
     /**
      * The mail sending protocol: mail, sendmail, smtp
      */
-    public string $protocol = 'mail';
+    public string $protocol = $_ENV['email.protocol'] ?? 'mail';
 
     /**
      * The server path to Sendmail.
      */
-    public string $mailPath = '/usr/sbin/sendmail';
+    public string $mailPath = $_ENV['email.sendmail_path'] ?? '/usr/sbin/sendmail';
 
     /**
      * SMTP Server Address
      */
-    public string $SMTPHost = '';
+    public string $SMTPHost = $_ENV['email.smpt_host'] ?? '';
 
     /**
      * SMTP Username
      */
-    public string $SMTPUser = '';
+    public string $SMTPUser = $_ENV['email.smtp_username'] ?? '';
 
     /**
      * SMTP Password
      */
-    public string $SMTPPass = '';
+    public string $SMTPPass = $_ENV['email.smtp_password'] ?? '';
 
     /**
      * SMTP Port
      */
-    public int $SMTPPort = 25;
+    public int $SMTPPort = $_ENV['email.smtp_port'] ?? 25;
 
     /**
      * SMTP Timeout (in seconds)
      */
-    public int $SMTPTimeout = 5;
+    public int $SMTPTimeout = $_ENV['email.smtp_timeout'] ?? 5;
 
     /**
      * Enable persistent SMTP connections
      */
-    public bool $SMTPKeepAlive = false;
+    public bool $SMTPKeepAlive = $_ENV['email.smtp_keepalive'] ?? false;
 
     /**
      * SMTP Encryption. Either tls or ssl
      */
-    public string $SMTPCrypto = 'tls';
+    public string $SMTPCrypto = $_ENV['email.smtp_encryption'] ?? 'tls';
 
     /**
      * Enable word-wrap
      */
-    public bool $wordWrap = true;
+    public bool $wordWrap = $_ENV['email.email_wordwrap'] ?? true;
 
     /**
      * Character count to wrap at
      */
-    public int $wrapChars = 76;
+    public int $wrapChars = $_ENV['email.email_wordwrpa_count'] ?? 76;
 
     /**
      * Type of mail, either 'text' or 'html'
      */
-    public string $mailType = 'text';
+    public string $mailType = $_ENV['email.type'] ?? 'text';
 
     /**
      * Character set (utf-8, iso-8859-1, etc.)
      */
-    public string $charset = 'UTF-8';
+    public string $charset = $_ENV['email.charset'] ?? 'UTF-8';
 
     /**
      * Whether to validate the email address
      */
-    public bool $validate = false;
+    public bool $validate = $_ENV['email.validate'] ?? false;
 
     /**
      * Email Priority. 1 = highest. 5 = lowest. 3 = normal
      */
-    public int $priority = 3;
+    public int $priority = $_ENV['email.priority'] ?? 3;
 
     /**
      * Newline character. (Use “\r\n” to comply with RFC 822)
      */
-    public string $CRLF = "\r\n";
+    public string $CRLF = $_ENV['email.newline_character'] ?? "\r\n";
 
     /**
      * Newline character. (Use “\r\n” to comply with RFC 822)
      */
-    public string $newline = "\r\n";
+    public string $newline = $_ENV['email.newline_character'] ?? "\r\n";
 
     /**
      * Enable BCC Batch Mode.
      */
-    public bool $BCCBatchMode = false;
+    public bool $BCCBatchMode = $_ENV['email.bcc_batch_mode'] ?? false;
 
     /**
      * Number of emails in each BCC batch
      */
-    public int $BCCBatchSize = 200;
+    public int $BCCBatchSize = $_ENV['email.bcc_batch_size'] ?? 200;
 
     /**
      * Enable notify message from server
      */
-    public bool $DSN = false;
+    public bool $DSN = $_ENV['email.dsn'] ?? false;
 }
