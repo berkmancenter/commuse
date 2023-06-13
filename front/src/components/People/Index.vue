b6f552683c6468f9d27fdea42696a35d34c63ce3
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2023-06-13 10:06:19 +0200

    Add search to people view

--- front/src/components/People/Index.vue
+++ front/src/components/People/Index.vue
@@ -2,50 +2,58 @@
   <div class="people-section">
     <h3 class="is-size-3">People</h3>
 
-      <div class="content people-section-content">
-          <div class="people-section-person"
-              v-for="(person, index) in $store.state.app.people"
-              :ref="'personRef_' + index">
-            <div class="is-size-4 mb-2">
-              <div>{{ person.first_name }}</div>
-              <div>{{ person.last_name }}</div>
-            </div>
+    <input
+      type="text"
+      v-model="searchTerm"
+      placeholder="Search"
+      class="input mb-4 people-section-search-input"
+    >
 
-            <div class="is-size-5 mb-4">
-              <div class="is-flex is-justify-content-center is-align-items-center" v-if="person.organization">
-                {{ person.organization }}
-              </div>
-            </div>
+    <div class="content people-section-content">
+        <div class="people-section-person"
+            v-for="(person, index) in filteredPeople"
+            :key="person.id"
+            :ref="'personRef_' + index">
+          <div class="is-size-4 mb-2">
+            <div>{{ person.first_name }}</div>
+            <div>{{ person.last_name }}</div>
+          </div>
 
-            <div class="people-section-avatar">
-              <img class="lazy"
-                  :data-src="person.image_url"
-                  >
-            </div>
-
-            <div class="mt-2 person-section-social is-flex">
-              <div v-if="person.bio" class="mr-2">
-                <a :href="person.twitter" target="_blank">
-                  <img src="@/assets/images/twitter.svg">
-                </a>
-              </div>
-              <div v-if="person.bio" class="mr-2">
-                <a :href="person.linkedin" target="_blank">
-                  <img src="@/assets/images/linkedin.svg">
-                </a>
-              </div>
-              <div v-if="person.bio" class="mr-2">
-                <a :href="person.mastodon" target="_blank">
-                  <img src="@/assets/images/mastodon.svg">
-                </a>
-              </div>
-            </div>
-
-            <div class="is-size-6 mt-2">
-              {{ person.bio }}
+          <div class="is-size-5 mb-4">
+            <div class="is-flex is-justify-content-center is-align-items-center" v-if="person.organization">
+              {{ person.organization }}
             </div>
           </div>
-      </div>
+
+          <div class="people-section-avatar">
+            <img class="lazy"
+                :data-src="person.image_url"
+            >
+          </div>
+
+          <div class="mt-2 person-section-social is-flex">
+            <div v-if="person.bio" class="mr-2">
+              <a :href="person.twitter" target="_blank">
+                <img src="@/assets/images/twitter.svg">
+              </a>
+            </div>
+            <div v-if="person.bio" class="mr-2">
+              <a :href="person.linkedin" target="_blank">
+                <img src="@/assets/images/linkedin.svg">
+              </a>
+            </div>
+            <div v-if="person.bio" class="mr-2">
+              <a :href="person.mastodon" target="_blank">
+                <img src="@/assets/images/mastodon.svg">
+              </a>
+            </div>
+          </div>
+
+          <div class="is-size-6 mt-2">
+            {{ person.bio }}
+          </div>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -57,9 +65,20 @@
     data() {
       return {
         lazyLoadInstance: null,
+        searchTerm: '',
       }
     },
-    computed: {},
+    computed: {
+      filteredPeople() {
+        const searchTerm = this.searchTerm.toLowerCase();
+
+        return this.$store.state.app.people.filter((person) => {
+          const searchText = `${person.first_name} ${person.last_name} ${person.bio}`.toLowerCase();
+
+          return searchText.includes(searchTerm);
+        });
+      },
+    },
     created() {
       this.initialDataLoad()
       this.initLazyLoad()
@@ -82,6 +101,13 @@
           unobserve_completed: true,
         })
         this.lazyLoadInstance.update()
+      },
+    },
+    watch: {
+      searchTerm() {
+        this.$nextTick(() => {
+          this.lazyLoadInstance.update()
+        })
       },
     },
   }
@@ -124,6 +150,10 @@
         width: 2rem;
         height: 2rem;
       }
+    }
+
+    .people-section-search-input {
+      width: 200px;
     }
   }
 </style>
