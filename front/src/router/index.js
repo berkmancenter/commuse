f65fc362b07095d765ca048ee6ad9f4e1419b89c
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2023-06-21 12:33:53 +0200

    Set page title dynamically

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const basePath = import.meta.env.VITE_BASE_PATH
+const appTitle = import.meta.env.VITE_APP_TITLE
+
 
 const router = createRouter({
   history: createWebHistory(basePath || '/'),
@@ -9,22 +11,36 @@ const router = createRouter({
       path: '/',
       component: () => import('@/components/Home/Index.vue'),
       name: 'home.index',
+      meta: {
+        title: 'Home'
+      },
     },
     {
       path: '/people',
       component: () => import('@/components/People/Index.vue'),
       name: 'people.index',
+      meta: {
+        title: 'People'
+      },
     },
     {
       path: '/profile',
       component: () => import('@/components/User/Profile.vue'),
       name: 'user-profile.index',
+      meta: {
+        title: 'Profile'
+      },
     },
   ]
 })
 
 router.afterEach(route => {
   window.scrollTo(0, 0)
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | ${appTitle}`
+  next()
 })
 
 export default router
