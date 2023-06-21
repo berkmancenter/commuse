9f05355bfea78820ce02338c3eb8ba1c79822030
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2023-06-21 18:03:08 +0200

    Add app title to login views

--- back/app/Views/Shield/layout.php
+++ back/app/Views/Shield/layout.php
@@ -9,13 +9,17 @@
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-    <link rel="icon" href="<?php echo $_ENV['app.baseURL'] ?>/favicon.svg">
+    <link rel="icon" href="<?= $_ENV['app.baseURL'] ?>/favicon.svg">
 
     <?= $this->renderSection('pageStyles') ?>
 </head>
 
 <body class="bg-light">
     <main role="main" class="container">
+        <div class="mt-4">
+            <h2 class="text-center"><?= $_ENV['app.appTitle'] ?? '' ?></h2>
+        </div>
+
         <?= $this->renderSection('main') ?>
     </main>
 
