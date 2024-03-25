11b272b9b17ae7c26b0a932dedb11b70fb05103f
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2024-03-25 11:15:44 +0100

    Put Newsimporter back in place

---
+++ back/app/Commands/NewsImport.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Commands;
+
+use CodeIgniter\CLI\BaseCommand;
+use CodeIgniter\CLI\CLI;
+use App\DataImporters\NewsImporter;
+
+class NewsImport extends BaseCommand
+{
+    protected $group       = 'custom';
+    protected $name        = 'news_import';
+    protected $description = 'Imports news data';
+
+    public function run(array $params)
+    {
+        $newsImporter = new NewsImporter();
+        $newsImporter->fetchContentItems();
+
+        CLI::write('Done');
+    }
+}
