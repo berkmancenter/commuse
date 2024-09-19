86c1cf783d2c19e30351b241e5713bb58645a0e5
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2024-09-19 22:30:40 +0200

    Add skeleton pattern to single person view

--- front/src/components/People/PersonDetails.vue
+++ front/src/components/People/PersonDetails.vue
@@ -1,139 +1,220 @@
 <template>
   <div class="people-section-details">
     <div class="people-section-details-side">
-      <div class="people-section-details-side-name-image">
-        <div class="is-size-4 mb-4 people-section-person-full-name">
-          <div>
-            {{ person.prefix }} {{ person.first_name }} {{ person.middle_name }} {{ person.last_name }}
-          </div>
-          <div class="is-size-6" v-if="person.preferred_pronouns">({{ person.preferred_pronouns }})</div>
-        </div>
-
-        <div class="people-section-details-side-image">
-          <img class="lazy" :src="imageSrc">
-        </div>
-      </div>
-
-      <div class="people-section-details-side-content">
-        <div class="people-section-details-side-content-items">
-          <div class="is-flex is-align-items-center" v-if="person.mobile_phone_number">
-            <img class="people-section-details-icon" :src="phoneIcon">
-            {{ person.mobile_phone_number }}
-          </div>
-
-          <div v-if="person.email">
-            <a :href="`mailto:${person.email}`" class="is-flex is-align-items-center">
-              <img class="people-section-details-icon" :src="emailIcon">
-              {{ person.email }}
-            </a>
-          </div>
-
-          <VTooltip distance="10" placement="left" v-if="address">
-            <div class="is-flex is-align-items-center">
-              <img class="people-section-details-icon" :src="homeIcon">
-              {{ address }}
+      <SkeletonPatternLoader :loading="loading">
+        <template v-slot:content>
+          <div class="people-section-details-side-name-image">
+            <div class="is-size-4 mb-4 people-section-person-full-name">
+              <div>
+                {{ person.prefix }} {{ person.first_name }} {{ person.middle_name }} {{ person.last_name }}
+              </div>
+              <div class="is-size-6" v-if="person.preferred_pronouns">({{ person.preferred_pronouns }})</div>
             </div>
 
-            <template #popper>
-              Permanent Residence
-            </template>
-          </VTooltip>
-
-          <VTooltip distance="10" placement="left" v-if="currentAddress">
-            <div class="is-flex is-align-items-center">
-              <img class="people-section-details-icon" :src="currentAddressIcon">
-              {{ currentAddress }}
+            <div class="people-section-details-side-image">
+              <img class="lazy" :src="`${apiUrl}/api/files/get/${this.person.image_url}`">
             </div>
+          </div>
 
-            <template #popper>
-              Current Location
-            </template>
-          </VTooltip>
+          <div class="people-section-details-side-content">
+            <div class="people-section-details-side-content-items">
+              <div class="is-flex is-align-items-center" v-if="person.mobile_phone_number">
+                <img class="people-section-details-icon" :src="phoneIcon">
+                {{ person.mobile_phone_number }}
+              </div>
 
-          <VTooltip distance="10" placement="left" v-if="person?.affiliation?.length > 0">
-            <div class="people-section-details-side-affiliation">
-              <div class="people-section-details-side-affiliation-item" v-for="affiliationItem in person.affiliation">
-                <div class="people-section-details-side-affiliation-item-icon">
-                  <img class="people-section-details-icon" :src="affiliateIcon">
+              <div v-if="person.email">
+                <a :href="`mailto:${person.email}`" class="is-flex is-align-items-center">
+                  <img class="people-section-details-icon" :src="emailIcon">
+                  {{ person.email }}
+                </a>
+              </div>
+
+              <VTooltip distance="10" placement="left" v-if="address">
+                <div class="is-flex is-align-items-center">
+                  <img class="people-section-details-icon" :src="homeIcon">
+                  {{ address }}
                 </div>
-                <div>
-                  {{ affiliationItem.tags.join(', ') }}, {{ calendarDateFormat(affiliationItem.from) }}-{{ calendarDateFormat(affiliationItem.to) }}
+
+                <template #popper>
+                  Permanent Residence
+                </template>
+              </VTooltip>
+
+              <VTooltip distance="10" placement="left" v-if="currentAddress">
+                <div class="is-flex is-align-items-center">
+                  <img class="people-section-details-icon" :src="currentAddressIcon">
+                  {{ currentAddress }}
                 </div>
+
+                <template #popper>
+                  Current Location
+                </template>
+              </VTooltip>
+
+              <VTooltip distance="10" placement="left" v-if="person?.affiliation?.length > 0">
+                <div class="people-section-details-side-affiliation">
+                  <div class="people-section-details-side-affiliation-item" v-for="affiliationItem in person.affiliation">
+                    <div class="people-section-details-side-affiliation-item-icon">
+                      <img class="people-section-details-icon" :src="affiliateIcon">
+                    </div>
+                    <div>
+                      {{ affiliationItem.tags.join(', ') }}, {{ calendarDateFormat(affiliationItem.from) }}-{{ calendarDateFormat(affiliationItem.to) }}
+                    </div>
+                  </div>
+                </div>
+
+                <template #popper>
+                  {{ affiliateFieldTitle('affiliation') }}(s)
+                </template>
+              </VTooltip>
+
+              <VTooltip distance="10" placement="left" v-if="person?.activeAffiliation?.length > 0">
+                <div class="people-section-details-side-affiliation">
+                  <div class="people-section-details-side-affiliation-item" v-for="activeAffiliationItem in person.activeAffiliation">
+                    <div class="people-section-details-side-affiliation-item-icon">
+                      <img class="people-section-details-icon" :src="affiliateIcon">
+                    </div>
+                    <div>
+                      {{ activeAffiliationItem.tags.join(', ') }}, {{ calendarDateFormat(activeAffiliationItem.from) }}-{{ calendarDateFormat(activeAffiliationItem.to) }}
+                    </div>
+                  </div>
+                </div>
+
+                <template #popper>
+                  {{ affiliateFieldTitle('activeAffiliation') }}
+                </template>
+              </VTooltip>
+
+              <div class="is-clearfix"></div>
+
+              <div class="people-section-details-side-links">
+                <div v-if="person.bio" class="people-section-details-side-link" @click="jumpToSection('group_bio')">Bio</div>
+                <div v-if="hasFields(myInformationGroup)" class="people-section-details-side-link" @click="jumpToSection('group_additional_information')">Additional Information</div>
+                <template v-for="group in customGroups" :key="group.machine_name">
+                  <div v-if="hasFields(group)" class="people-section-details-side-link" @click="jumpToSection(`group_${group.machine_name}`)">{{ group.title }}</div>
+                </template>
+              </div>
+
+              <div class="mt-2 people-section-details-last-updated">Last updated: {{ person.updated_at }} UTC</div>
+
+              <div class="people-section-details-admin-actions" v-if="$store.state.user.currentUser.admin">
+                <hr>
+
+                <h4 class="is-size-5 mt-2">Admin</h4>
+
+                <ActionButton class="mt-2" buttonText="Edit profile" @click="$router.push({ name: 'user-profile-admin.index', params: { id: person.user_id } })" :icon="editIcon"></ActionButton>
               </div>
             </div>
+          </div>
+        </template>
 
-            <template #popper>
-              {{ affiliateFieldTitle('affiliation') }}(s)
-            </template>
-          </VTooltip>
-
-          <VTooltip distance="10" placement="left" v-if="person?.activeAffiliation?.length > 0">
-            <div class="people-section-details-side-affiliation">
-              <div class="people-section-details-side-affiliation-item" v-for="activeAffiliationItem in person.activeAffiliation">
-                <div class="people-section-details-side-affiliation-item-icon">
-                  <img class="people-section-details-icon" :src="affiliateIcon">
-                </div>
-                <div>
-                  {{ activeAffiliationItem.tags.join(', ') }}, {{ calendarDateFormat(activeAffiliationItem.from) }}-{{ calendarDateFormat(activeAffiliationItem.to) }}
-                </div>
+        <template v-slot:skeleton>
+          <div class="content people-section-content">
+            <div class="ssc-wrapper">
+              <div class="ssc-head-line w-60 mb-4"></div>
+              <div class="ssc-square people-section-details-side-ssc-avatar mb-4"></div>
+              <div class="align-center flex justify-between mb-4" v-for="n in 4" :key="n">
+                <div class="ssc-line w-20"></div>
+                <div class="ssc-line w-70"></div>
               </div>
+              <div class="ssc-head-line w-40 mb-4"></div>
+              <div class="ssc-head-line w-60 mb-4"></div>
+              <div class="ssc-head-line w-50 mb-4"></div>
+              <div class="ssc-head-line w-70 mb-4"></div>
+              <div class="ssc-line w-100"></div>
             </div>
-
-            <template #popper>
-              {{ affiliateFieldTitle('activeAffiliation') }}
-            </template>
-          </VTooltip>
-
-          <div class="is-clearfix"></div>
-
-          <div class="people-section-details-side-links">
-            <div v-if="person.bio" class="people-section-details-side-link" @click="jumpToSection('group_bio')">Bio</div>
-            <div v-if="hasFields(myInformationGroup)" class="people-section-details-side-link" @click="jumpToSection('group_additional_information')">Additional Information</div>
-            <template v-for="group in customGroups" :key="group.machine_name">
-              <div v-if="hasFields(group)" class="people-section-details-side-link" @click="jumpToSection(`group_${group.machine_name}`)">{{ group.title }}</div>
-            </template>
           </div>
-
-          <div class="mt-2 people-section-details-last-updated">Last updated: {{ person.updated_at }} UTC</div>
-
-          <div class="people-section-details-admin-actions" v-if="$store.state.user.currentUser.admin">
-            <hr>
-
-            <h4 class="is-size-5 mt-2">Admin</h4>
-
-            <ActionButton class="mt-2" buttonText="Edit profile" @click="$router.push({ name: 'user-profile-admin.index', params: { id: person.user_id } })" :icon="editIcon"></ActionButton>
-          </div>
-        </div>
-      </div>
+        </template>
+      </SkeletonPatternLoader>
     </div>
 
     <div class="box people-section-details-content">
-      <div class="notification is-warning" v-if="person.public_profile === 'f'">
-        {{ $store.state.systemSettings.publicSystemSettings?.PublicProfileWarningShowProfile?.value }}
-      </div>
-
-      <div class="people-section-details-content-other">
-        <div v-if="person.bio" ref="group_bio">
-          <div class="panel">
-            <p class="panel-heading">
-              Bio
-            </p>
-            <div class="panel-block">
-              <ShowMore :text="person.bio"></ShowMore>
-            </div>
+      <SkeletonPatternLoader :loading="loading">
+        <template v-slot:content>
+          <div class="notification is-warning" v-if="person.public_profile === 'f'">
+            {{ $store.state.systemSettings.publicSystemSettings?.PublicProfileWarningShowProfile?.value }}
           </div>
-        </div>
 
-        <div v-if="hasFields(myInformationGroup)" ref="group_additional_information">
-          <PersonDetailsGroup :group="myInformationGroup" :person="person" custom-group-title="Additional Information"></PersonDetailsGroup>
-        </div>
+          <div class="people-section-details-content-other">
+            <div v-if="person.bio" ref="group_bio">
+              <div class="panel">
+                <p class="panel-heading">
+                  Bio
+                </p>
+                <div class="panel-block">
+                  <ShowMore :text="person.bio"></ShowMore>
+                </div>
+              </div>
+            </div>
 
-        <template v-for="group in customGroups" :key="group.machine_name">
-          <div :ref="`group_${group.machine_name}`" v-if="hasFields(group)">
-            <PersonDetailsGroup :group="group" :person="person"></PersonDetailsGroup>
+            <div v-if="hasFields(myInformationGroup)" ref="group_additional_information">
+              <PersonDetailsGroup :group="myInformationGroup" :person="person" custom-group-title="Additional Information"></PersonDetailsGroup>
+            </div>
+
+            <template v-for="group in customGroups" :key="group.machine_name">
+              <div :ref="`group_${group.machine_name}`" v-if="hasFields(group)">
+                <PersonDetailsGroup :group="group" :person="person"></PersonDetailsGroup>
+              </div>
+            </template>
           </div>
         </template>
-      </div>
+
+        <template v-slot:skeleton>
+          <div class="ssc-wrapper">
+            <div class="ssc-head-line w-100 mb-4"></div>
+            <div class="ssc-square w-100 mb-6"></div>
+
+            <div class="ssc-head-line w-100 mb-4"></div>
+            <div class="ssc-line w-40 mb-4"></div>
+            <div class="ssc-line w-100 mb-6"></div>
+
+            <div class="ssc-head-line w-100 mb-4"></div>
+            <div class="ssc-line w-40 mb-4"></div>
+            <div class="ssc-line w-100 mb-4"></div>
+            <div class="ssc-line w-100 mb-6"></div>
+
+            <div class="ssc-head-line w-100 mb-4"></div>
+            <div class="ssc-line w-40 mb-4"></div>
+            <div class="flex is-align-items-flex-start mb-6">
+              <div class="ssc-line mr-2 w-10"></div>
+              <div class="ssc-line mr-2 w-20"></div>
+              <div class="ssc-line mr-2 w-10"></div>
+              <div class="ssc-line mr-2 w-15"></div>
+              <div class="ssc-line mr-2 w-20"></div>
+              <div class="ssc-line mr-2 w-10"></div>
+              <div class="ssc-line mr-2 w-20"></div>
+            </div>
+
+            <div class="ssc-head-line w-100 mb-4"></div>
+            <div class="ssc-line w-30 mb-4"></div>
+            <div class="flex is-align-items-flex-start mb-4">
+              <div class="ssc-line mr-2 w-20"></div>
+              <div class="ssc-line mr-2 w-30"></div>
+              <div class="ssc-line mr-2 w-10"></div>
+              <div class="ssc-line mr-2 w-15"></div>
+              <div class="ssc-line mr-2 w-20"></div>
+            </div>
+            <div class="flex is-align-items-flex-start mb-6">
+              <div class="ssc-line mr-2 w-10"></div>
+              <div class="ssc-line mr-2 w-20"></div>
+              <div class="ssc-line mr-2 w-10"></div>
+              <div class="ssc-line mr-2 w-15"></div>
+              <div class="ssc-line mr-2 w-20"></div>
+              <div class="ssc-line mr-2 w-10"></div>
+              <div class="ssc-line mr-2 w-20"></div>
+            </div>
+
+            <div class="ssc-head-line w-100 mb-4"></div>
+            <div class="ssc-line w-40 mb-4"></div>
+            <div class="ssc-line w-100 mb-4"></div>
+            <div class="ssc-line w-100 mb-4"></div>
+            <div class="ssc-line w-100 mb-4"></div>
+            <div class="ssc-line w-100 mb-4"></div>
+            <div class="ssc-line w-100 mb-6"></div>
+          </div>
+        </template>
+      </SkeletonPatternLoader>
     </div>
   </div>
 </template>
@@ -153,6 +234,7 @@
   import { getMultiFieldValue } from '@/lib/fields/multi.js'
   import { calendarDateFormat } from '@/lib/time_stuff.js'
   import LazyLoad from 'vanilla-lazyload'
+  import SkeletonPatternLoader from '@/components/Shared/SkeletonPatternLoader.vue'
 
   export default {
     name: 'PersonDetails',
@@ -171,12 +253,14 @@
         getMultiFieldValue,
         calendarDateFormat,
         lazyLoadInstance: null,
+        loading: true,
       }
     },
     components: {
       PersonDetailsGroup,
       ShowMore,
       ActionButton,
+      SkeletonPatternLoader,
     },
     created() {
       this.initialDataLoad()
@@ -191,15 +275,6 @@
       },
       currentAddress() {
         return [this.person.current_city, this.person.current_state, this.person.current_country].filter(n => n).join(', ')
-      },
-      imageSrc() {
-        let src = this.profileFallbackImage
-
-        if (this.person.image_url) {
-          src = `${this.apiUrl}/api/files/get/${this.person.image_url}`
-        }
-
-        return src
       },
       myInformationGroup() {
         let group = this.profileStructure?.filter((group) => { return group['machine_name'] == 'my_information' })[0]
@@ -223,6 +298,7 @@
         this.loadProfileStructure()
         await this.loadPerson()
         this.initImgLazyLoad()
+        this.loading = false
       },
       async loadPerson() {
         const response = await this.$store.dispatch('people/fetchPerson', this.$route.params.id)
@@ -310,7 +386,7 @@
 
     .people-section-details-side {
       position: fixed;
-      max-width: 300px;
+      width: 300px;
       padding: 1rem;
       background-color: var(--super-light-color);
       overflow-y: auto;
@@ -365,6 +441,10 @@
         @media screen and (max-width: 768px) {
           margin-left: 0;
         }
+      }
+
+      .people-section-details-side-ssc-avatar {
+        height: 270px;
       }
     }
 
