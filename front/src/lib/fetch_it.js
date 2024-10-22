b47d72af00b1e6b193c7abfba9fe915b043ddaa4
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2024-10-22 11:16:12 +0100

    Handle aborted json fetching in fetch wrapper

--- front/src/lib/fetch_it.js
+++ front/src/lib/fetch_it.js
@@ -15,30 +15,38 @@ const fetchIt = async (url, options = {}) => {
   try {
     const response = await fetch(url, options)
 
+    // Redirect if login is required
     if (response.redirected && response.url.includes('/login')) {
       window.location.href = response.url
-
       return Promise.reject('Redirecting to login')
     }
 
+    // Check for non-200 responses
     if (!response.ok) {
       globals.awn.warning('Something went wrong, try again.')
-
       return Promise.reject('Fetch failed with status ' + response.status)
     }
 
-    const data = await response.json().catch(() => {
+    // Use .text() and then manually parse JSON to handle incomplete/broken responses better
+    const text = await response.text()
+    
+    let data
+    try {
+      data = JSON.parse(text)
+    } catch (err) {
       globals.awn.warning('Invalid JSON response.')
-
       return Promise.reject('Invalid JSON response')
-    })
+    }
 
     return Promise.resolve(data)
   } catch (error) {
-    if (error.name !== 'AbortError') {
-      globals.awn.warning('Something went wrong, try again.')
+    // Ignore AbortError and show no warning, since it's intentional
+    if (error.name === 'AbortError') {
+      return Promise.reject('Fetch aborted')
     }
 
+    // Handle other errors
+    globals.awn.warning('Something went wrong, try again.')
     return Promise.reject(error)
   }
 }
